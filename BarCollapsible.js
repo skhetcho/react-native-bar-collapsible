67b148a6302c14c549e5799cf0ee04939beba139
Carolina Aguilar <caroagse@gmail.com>
2016-05-06 15:16:16 -0600

    new: dev: Initial repository layout.

---
+++ BarCollapsible.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import React, { View } from 'react-native';
+
+class BarCollapsible extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <View/>
+        );
+    }
+}
+
+module.exports = BarCollapsible;
