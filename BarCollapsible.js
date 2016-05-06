298a0a047ceb2af5c74d4a8825ab4ba1460c273d
Carolina Aguilar <caroagse@gmail.com>
2016-05-06 17:40:57 -0600

    fix: dev: Renamed styles file.

--- BarCollapsible.js
+++ BarCollapsible.js
@@ -4,7 +4,7 @@ import React, { Animated, View, Text, TouchableHighlight } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-import styles from '../styles/collapsible'
+import styles from '../styles/style'
 
 class BarCollapsible extends React.Component {
     constructor(props) {
