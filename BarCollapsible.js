83a935528dc86b6afeb91d307151ab278af66beb
Souren <khetcho@gmail.com>
2019-06-11 04:58:11 -0700

    fixed PropTypes deprecated issue

--- BarCollapsible.js
+++ BarCollapsible.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React, { Component } from 'react';
+import ViewPropTypes from 'react-native';
 import PropTypes from 'prop-types';
 import { 
     Animated, 
@@ -26,7 +27,7 @@ class BarCollapsible extends Component {
     }
 
     static propTypes = {
-        style: View.propTypes.style,
+        style: ViewPropTypes.style,
         titleStyle: Text.propTypes.style,
         tintColor: PropTypes.string,
     }
